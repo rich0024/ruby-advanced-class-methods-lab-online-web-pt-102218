d1ac9f33794ef654b79b1272d32f16770db2a914
Richard Yance <yance0022@gmail.com>
2018-12-08 17:30:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,25 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.name
+
+  end
+
   def self.all
     @@all
   end
 
+  def self.create
+    self.new.save
+  end
+
+  def self.new_by_name(song_name)
+    song_name = @name
+  end
+
   def save
     self.class.all << self
+    self
   end
 
 end
