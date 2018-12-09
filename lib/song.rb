c69ceb2f421ed85044121c839bed06e6f837855b
Richard Yance <yance0022@gmail.com>
2018-12-09 05:08:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,24 @@ class Song
     @@all
   end
 
+  def name
+    @name = name
+    name
+  end
+
+  def self.create
+    self.new.save
+  end
+
+  def self.new_by_name(song_name)
+    song_name = @name
+    @name = name
+    self.name
+  end
+
   def save
     self.class.all << self
+    self
   end
 
 end
